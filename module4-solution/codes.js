7f8faac4de54c9a59e42fc9322d194c9f8492644
Amin <m.a.moslemi@gmail.com>
2023-02-20 12:18:43 +0330

    del m4

--- module4-solution/codes.js
+++
@@ -1,2 +0,0 @@
-amirHello.sayHello()
-aminHi.sayHi()
--- module4-solution/codes1.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-    var aminHi = {}
-    aminHi.name = "Amin"
-    var greeting = "Hi"
-    aminHi.sayHi = function () {
-        console.log(greeting + " " + aminHi.name);
-    }
-    window.aminHi = aminHi
-})(window)
